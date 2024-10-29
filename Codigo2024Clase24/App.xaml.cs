d2a84aee7a7836f8f03c2c29133899a59fc92b43
Hugo Torrico <torricomarquezh@gmail.com>
2024-10-28 20:32:04 -0500

    Agregar archivos de proyecto.

---
+++ Codigo2024Clase24/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Codigo2024Clase24
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Codigo2024Clase24/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codigo2024Clase24
+{
+    public class MainWindowViewModel
+    {
+        public int Valor1 { get; set; }
+        public int Valor2 { get; set; }
+
+        public int ResultadoSuma { get; set; }
+        public void Sumar()
+        {
+            ResultadoSuma = Valor1 + Valor2;
+        }
+    }
+}

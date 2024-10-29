0f157e5d77b62bae981bebe5cfd834ab880c25ad
Hugo Torrico <torricomarquezh@gmail.com>
2024-10-28 21:51:40 -0500

    ejemplo de trabajador

---
+++ Codigo2024Clase24/ViewModels/TrabajadorViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codigo2024Clase24
+{
+    public class TrabajadorViewModel
+    {
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+
+        public int TipoContrato { get; set; }
+        public double SueldoBruto { get; set; }
+        public double SueldoNeto { get; set; }
+
+        public double Impuesto { get; set; }
+
+        private void CalcularImpuesto()
+        {
+            if (TipoContrato==1)
+            {
+                //Lógica de 4TA Categoria
+                double SueldoAnual = 0;
+                SueldoAnual = SueldoBruto * 12;
+
+                if (SueldoAnual>3500)
+                {
+                    Impuesto = SueldoBruto * 0.08;
+                }
+
+
+            }
+            else
+            {
+
+            }
+
+
+        }
+
+
+
+    }
+}
